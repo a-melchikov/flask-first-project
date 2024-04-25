95d9b9951348fad5916a2e165774c5583d3307d9
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-04-25 21:21:11 +0400

    Connecting external resources and working with forms

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, url_for
+from crypt import methods
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [
+    {"name": "Установка", "url": "install-flask"},
+    {"name": "Первое приложение", "url": "first-app"},
+    {"name": "Обратная связь", "url": "contact"},
+]
 
 
 @app.route("/")
@@ -17,15 +22,17 @@ def about():
     return render_template("about.html", title="О сайте", menu=menu)
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+    return render_template("contact.html", title="Обратная связь", menu=menu)
 
 
-with app.test_request_context():
-    print(url_for("index"))
-    print(url_for("about"))
-    print(url_for("profile", username="user1234"))
+# with app.test_request_context():
+#     print(url_for("index"))
+#     print(url_for("about"))
+#     print(url_for("profile", username="user1234"))
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
