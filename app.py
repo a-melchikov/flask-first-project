0e043a7836bb2332d1a0ed01f14e270afbcf1868
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-04-25 18:25:34 +0400

    Using Site Page templates

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
 
 @app.route("/index")
 @app.route("/")
 def index():
-    return "index"
+    return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
-    return "<h1>О сайте</h1>"
+    return render_template("about.html", title="О сайте")
 
 
 if __name__ == "__main__":
