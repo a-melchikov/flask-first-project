50066fda04f0e1949442f53b8383355e8cdabdc9
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-04-25 18:57:03 +0400

    url_for function and URL variables

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 menu = ["Установка", "Первое приложение", "Обратная связь"]
 
-@app.route("/index")
+
 @app.route("/")
 def index():
+    print(url_for("index"))
     return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="О сайте")
+    print(url_for("about"))
+    return render_template("about.html", title="О сайте", menu=menu)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("about"))
+    print(url_for("profile", username="user1234"))
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
